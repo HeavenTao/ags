c7eaf490897a88c5293ddd4de213cba7c75f9615
HeavenTao <542248377@qq.com>
2024-07-20 15:16:09 +0800

    1.sync

--- widgets/volume.ts
+++ widgets/volume.ts
@@ -1,16 +1,25 @@
 import findIcon from "lib/iconUtils"
 function Volume() {
-    return Widget.Box({
-        className: "box",
-        spacing: 8,
-        children: [
-            Widget.Icon({
-                icon: findIcon("audio-on", "panel", "16")
-            }),
-            Widget.Label({
-                label: "90%"
-            })
-        ]
+    return Widget.EventBox({
+        onScrollUp: () => {
+            console.log("UP")
+        },
+        onScrollDown: () => {
+            console.log("Down")
+        },
+        child: Widget.Box({
+            className: "box",
+            spacing: 8,
+            children: [
+                Widget.Icon({
+                    icon: findIcon("audio-on", "panel", "16")
+                }),
+                Widget.Label({
+                    className: "volume",
+                    label: "90%"
+                })
+            ]
+        })
     })
 }
 
